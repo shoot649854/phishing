60059b2f839e21414582f850091fd90cbd51fdd9
shotomorisaki <jojoto8845@icloud.com>
2023-01-07 09:02:31 -0800

    Update index.php

--- index.php
+++ index.php
@@ -19,28 +19,28 @@
 <?php
 
 echo $_SERVER['HTTP_USER_AGENT'];
-// $yo = date("Y/m/d H:i:s");
-// $who = $_SERVER['REMOTE_ADDR'];
-// $where = $_SERVER["HTTP_REFERER"];
-// $where = rawurldecode($where);
-// mb_convert_encoding($where, "UTF-8", "EUC-JP");
-// $r_add = gethostbyaddr($_SERVER["REMOTE_ADDR"]);
-// $browser = $_SERVER["HTTP_USER_AGENT"];
+$yo = date("Y/m/d H:i:s");
+$who = $_SERVER['REMOTE_ADDR'];
+$where = $_SERVER["HTTP_REFERER"];
+$where = rawurldecode($where);
+mb_convert_encoding($where, "UTF-8", "EUC-JP");
+$r_add = gethostbyaddr($_SERVER["REMOTE_ADDR"]);
+$browser = $_SERVER["HTTP_USER_AGENT"];
 
-// $fp = fopen("data_index/index_data.txt","a");
-// flock($fp,2);
-// fputs($fp,"日時：".$yo);
-// fputs($fp,",");
-// fputs($fp,"鯖：".$who);
-// fputs($fp,",");
-// fputs($fp,"鯖アド：".$r_add);
-// fputs($fp,",");
-// fputs($fp,"どこから：".$where);
-// fputs($fp,",");
-// fputs($fp,"ブラウザ：".$browser);
-// fputs($fp,"\n");
-// flock($fp,3);
-// fclose($fp); 
+$fp = fopen("data_index/index_data.txt","a");
+flock($fp,2);
+fputs($fp,"日時：".$yo);
+fputs($fp,",");
+fputs($fp,"鯖：".$who);
+fputs($fp,",");
+fputs($fp,"鯖アド：".$r_add);
+fputs($fp,",");
+fputs($fp,"どこから：".$where);
+fputs($fp,",");
+fputs($fp,"ブラウザ：".$browser);
+fputs($fp,"\n");
+flock($fp,3);
+fclose($fp); 
 
 ?>
 </body>
